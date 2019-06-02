b861771f5aaeee9c1387a4b4d2647bc5ebe553ad
Fernando Henrique <fernando.hnrq@gmail.com>
2019-06-02 16:36:55 -0300

    Criando UserResource

--- src/main/java/com/fernandohza/helpdesk/service/impl/UserServiceImpl.java
+++ src/main/java/com/fernandohza/helpdesk/service/impl/UserServiceImpl.java
@@ -24,7 +24,6 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User createOrUpdate(User user) {
-		// TODO Auto-generated method stub
 		return this.userRepository.save(user);
 	}
 
